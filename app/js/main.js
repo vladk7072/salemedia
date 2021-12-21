ecdc4340a8cc14cf721af47fe610efec3e097e29
vladk7072 <vladk7072@gmail.com>
2021-12-21 18:18:02 +0200

    21.12.2021

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,29 @@
-$(function(){
+$(function () {
   const topSwiper = new Swiper(".top-slider__slider", {
+    speed: 500,
     navigation: {
-      nextEl: '.top-slider__arrow-next',
-      prevEl: '.top-slider__arrow-prev',
+      nextEl: ".top-slider__arrow-next",
+      prevEl: ".top-slider__arrow-prev",
     },
     pagination: {
-      el: '.top-slider__pagination',
+      el: ".top-slider__pagination",
     },
   });
+
+  const closeBanner = document.querySelector(".fixed-banner"),
+  closeBtn = document.querySelector(".fixed-banner__close-img"),
+  footer = document.querySelector(".footer");
+
+  closeBtn.addEventListener("click", () => {
+    closeBanner.style.display = "none",
+    footer.style.paddingBottom = "30px"
+  });
+
+  const sendBtns = document.querySelector(".sendPopup__social-btns");
+
+  $(".sendPopup__social-btn").click(function () {
+    $(".sendPopup__social-btn").removeClass("sendPopup__social-btn--active");
+    $(this).addClass("sendPopup__social-btn--active");
+  });
+  
 });
